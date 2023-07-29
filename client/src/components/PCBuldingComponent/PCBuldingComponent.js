76dc0a2168d88897eb0143e9cc68c42d9eae7210
hasibul1670 <hasibulislam1670@gmail.com>
2023-07-29 18:13:07 +0600

    feat : pc builder Page Partially Done [Design]

--- client/src/components/PCBuldingComponent/PCBuldingComponent.js
+++ client/src/components/PCBuldingComponent/PCBuldingComponent.js
@@ -5,7 +5,6 @@ const PCBuldingComponent = ({componentType,demoText,handleChooseComponent,BsCpu,
 
   const {  componentName, price } = componentType;
 
-console.log('Hello',componentType);
   return (
     <div className="card w-full py-2 px-5  h-20 bg-base-300  shadow-xl">
     <div className="flex justify-between  items-center  ">
@@ -16,13 +15,13 @@ console.log('Hello',componentType);
         <div>
           <p className="text-sm font-bold text-pink-700 ">{title}</p>
           <p className="text-xl font-bold text-cyan-700">
-          {  componentName ?    componentName : demoText}
+          {  componentName ?  <span className='text-xl text-blue-700'>{componentName}</span>   : <span className='text-sm  text-teal-700'>{demoText}</span>}
      
           </p>
         </div>
       </div>
       <div className='flex'>
-      <p className="text-sm font-bold mr-2 text-pink-700">
+      <p className="text-sm font-bold mr-2 mt-1 text-pink-700">
             {price || 0}
           </p>
         <button
